3597dfc4217fb8860ac58804a5da7c40a6db8cb1
Daniel Costa <danielcc.jp@gmail.com>
2022-11-08 19:25:47 -0300

    feat: implement scraping for movies titles

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let url = "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100";
+
+    let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    let document = scraper::Html::parse_document(&response);
+
+    let title_selector = scraper::Selector::parse("h3.lister-item-header>a").unwrap();
+
+    let titles = document.select(&title_selector).map(|x| x.inner_html());
+
+    titles
+        .zip(1..101)
+        .for_each(|(item, number)| println!("{number}. {item}"));
+}
